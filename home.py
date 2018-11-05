09681b215dfb53e9f7b92a000b7b454ea13c5bcc
Priyendu <priyendu.mori@gmail.com>
2018-11-05 13:11:36 +0530

    post answer partially done

--- home.py
+++ home.py
@@ -279,7 +279,6 @@ def ask_question_1():
 
 	return redirect(url_for('.index'))
 
-
 @app.route("/todo")
 def todo():
 	return render_template('todo.html')
@@ -302,6 +301,15 @@ def contact_us_1():
 	cu=contact_us(cu_name=name,cu_email_id=email,cu_mobile_no=mobile,cu_msg=message)
 	db.session.add(cu)
 	db.session.commit()
+
+@app.route("/post_answer",methods=['POST'])
+def post_answer():
+	ans_content = request.form['editordata']
+	ans_date = datetime.utcnow()
+	ans = answer(ans_content=ans_content,votes=0,user_id=1,question_id=1,ans_date=ans_date) 
+	db.session.add(ans)
+	db.session.commit()
+	return redirect(url_for('que_page'))
 
 if __name__=='__main__':
 	app.run(port=5000,debug=True,threaded=True,host="127.0.0.1")
